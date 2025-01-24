b1a81412a6a6c2c086e056d66b6ad62a53ac71df
Iván E. Hernández T. <ivan@iamieht.com>
2025-01-24 11:36:39 +0100

    Create 5.input.py
    Input

---
+++ PY100/UoW/5.input.py
@@ -0,0 +1,6 @@
+# Write a program that reads one line of input, and prints out that same line two times. For example, if the input is Echo the output should be
+# Echo
+# Echo
+inputStr = input()
+print(inputStr)
+print(inputStr)
