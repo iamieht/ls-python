a6f81d5fba646711394a59251534b3b5adfc1823
Iván E. Hernández T. <ivan@iamieht.com>
2025-01-24 08:38:58 +0100

    Create 1E_errors.py
    Errors

---
+++ PY100/UoW/1E_errors.py
@@ -0,0 +1,23 @@
+# Ex1: Fix the syntax error in the following program, so that it prints out the sum of all the numbers from 1 to 10. You can change at most one character.
+# print(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 +)
+print(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10)
+
+# Ex2: Fix the run-time error in the following program, so that it prints out Hello on the first line and Joe on the second line. You can change at most two characters.
+# print("Hello")
+# username = Joe
+# print(username)
+print("Hello")
+username = "Joe"
+print(username)
+
+# Ex3: You are going shopping for meat and milk, but there is tax. You buy $2.00 of milk and $4.00 of meat, and the tax rate is 3%. Print out the total cost of your groceries
+# meatPrice = 4.00
+# meatTax = 0.03 * meatPrice
+# milkPrice = 2.00
+# milkTax = 0.03 * milkPrice
+# print(meatTax + meatPrice + milkTax + meatPrice)
+meatPrice = 4.00
+meatTax = 0.03 * meatPrice
+milkPrice = 2.00
+milkTax = 0.03 * milkPrice
+print(meatTax + meatPrice + milkTax + milkPrice)
