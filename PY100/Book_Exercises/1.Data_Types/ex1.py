cd834629b6d861250c056825bd793cf1856568b2
Iván E. Hernández T. <ivan@iamieht.com>
2025-01-27 17:07:03 +0100

    Ex1

---
+++ PY100/Book_Exercises/1.Data_Types/ex1.py
@@ -0,0 +1,11 @@
+# Identify the data type or class for each of the following values:
+'True'          # String        class: str          
+False           # Boolean       class: bool 
+(1, 2, 3)       # Tuple         class: tuple
+1.5             # Float         class: float    
+[1, 2, 3]       # List          class list
+2               # Integer       class: int
+range(5)        # Range         class: range
+{1, 2, 3}       # Set           class: set       
+None            # NoneType      class: NoneType
+{'foo': 'bar'}  # Dictionary    class: dict
