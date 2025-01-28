89dc02aad17e1369a4c2df0f698234ae9dcda973
Iván E. Hernández T. <ivan@iamieht.com>
2025-01-28 18:13:51 +0100

    Variables - Ex3

---
+++ PY100/Book_Exercises/3.Variables/ex3.py
@@ -0,0 +1,13 @@
+# Classify the following potential constant names as idiomatic, non-idiomatic, or illegal. For the non-idiomatic and illegal names, explain your choice.
+
+'''
+index           # non-idiomatic
+CatName         # non-idiomatic
+snake_case      # non-idiomatic
+LAZY_DOG3       # idiomatic
+1ST             # illegal
+operand2        # non-idiomatic
+BIG_NUMBER      # idiomatic
+Π               # non-idiomatic
+'''
+
