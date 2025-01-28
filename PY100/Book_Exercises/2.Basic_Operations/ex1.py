30e0e8143a9065ee059fcb3d7339657eadb5e0b3
Iván E. Hernández T. <ivan@iamieht.com>
2025-01-28 13:22:31 +0100

    Create ex1.py
    Basic Operations : Ex1

---
+++ PY100/Book_Exercises/2.Basic_Operations/ex1.py
@@ -0,0 +1,9 @@
+# Concatenate two strings, one with your first name and one with your last, to create a new string with your full name as its value. For example, if your name is John Doe, you should combine 'John' and 'Doe' to get 'John Doe'.
+
+# Option 1
+print("John" + " Doe")
+
+# Option 2
+name = "John"
+last_name = "Doe"
+print(f'{name} {last_name}')
