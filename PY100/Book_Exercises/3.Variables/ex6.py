248e633c46b9cef16f6a38af9bfad50a679c3d85
Iván E. Hernández T. <ivan@iamieht.com>
2025-01-28 18:36:26 +0100

    Variables - Ex6

---
+++ PY100/Book_Exercises/3.Variables/ex6.py
@@ -0,0 +1,9 @@
+# Write a program named age.py that includes someone's age and then calculates and reports the future age 10, 20, 30, and 40 years from now. 
+
+age = int(input('How old are you? '))
+
+print(f'You are {age} years old.')
+print(f'In {10} years, you will be {10 + age} years old.')
+print(f'In {20} years, you will be {20 + age} years old.')
+print(f'In {30} years, you will be {30 + age} years old.')
+print(f'In {40} years, you will be {40 + age} years old.')
