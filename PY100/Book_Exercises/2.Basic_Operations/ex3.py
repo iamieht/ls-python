5a7a4a6b04011055fee151ffc45a78693386828f
Iván E. Hernández T. <ivan@iamieht.com>
2025-01-28 13:40:30 +0100

    Create ex3.py
    Ex3

---
+++ PY100/Book_Exercises/2.Basic_Operations/ex3.py
@@ -0,0 +1,4 @@
+# What does the following code do? Why?
+print('5' + '10')
+
+# Answer: The code outputs the string object '510' as the plus operator used with strings operands, performs concantenation and returns a new string.
