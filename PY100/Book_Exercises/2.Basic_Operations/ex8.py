640fc88feddc960264e31f5bec96445d5c561806
Iván E. Hernández T. <ivan@iamieht.com>
2025-01-28 14:00:03 +0100

    Create ex8.py
    Ex8

---
+++ PY100/Book_Exercises/2.Basic_Operations/ex8.py
@@ -0,0 +1,4 @@
+# To what value does the following expression evaluate?
+'12' < '9'
+
+# Answer: The expression evaluates as True, Python performs a character-by-character comparison from left to right when comparing string values. Thus, on the first comparison, it determines that '1' < '9', so '12' must be less than '9'.
