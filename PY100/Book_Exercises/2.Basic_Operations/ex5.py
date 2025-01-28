7d3cc0f332b2f43b6761a84b5aa8c3ffe3371259
Iván E. Hernández T. <ivan@iamieht.com>
2025-01-28 13:46:48 +0100

    Create ex5.py
    Ex5

---
+++ PY100/Book_Exercises/2.Basic_Operations/ex5.py
@@ -0,0 +1,4 @@
+# Will an error occur if you try to access a list element with an index greater than or equal to the list's length? For example:
+foo = ['a', 'b', 'c']
+# will this result in an error?: Yes!, it will result in an index out of range error, as there is no element with index 3 in the foo list.
+print(foo[3])
