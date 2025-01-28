2afd3c68d45d5bec4cb6d5e67c14cdf2fea5f3a2
Iván E. Hernández T. <ivan@iamieht.com>
2025-01-28 18:15:42 +0100

    Variables - Ex4

---
+++ PY100/Book_Exercises/3.Variables/ex4.py
@@ -0,0 +1,11 @@
+# Classify the following potential class names as idiomatic, non-idiomatic, or illegal. For the non-idiomatic and illegal names, explain your choice.
+
+'''
+index           # non-idiomatic
+CatName         # idiomatic
+Lazy_Dog        # non-idiomatic
+1ST             # illegal
+operand2        # non-idiomatic    
+BigNumber3      # idiomatic
+Πi              # non-idiomatic
+'''
