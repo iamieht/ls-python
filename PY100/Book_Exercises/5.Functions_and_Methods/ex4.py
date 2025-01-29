2c62a2779efa577c9ee8aa1dc5978da0f1055087
Iván E. Hernández T. <ivan@iamieht.com>
2025-01-29 18:41:31 +0100

    Functions and Methods: Ex4

---
+++ PY100/Book_Exercises/5.Functions_and_Methods/ex4.py
@@ -0,0 +1,15 @@
+def multiply_numbers(num1, num2, num3):
+    result = num1 * num2 * num3
+    return result
+
+product = multiply_numbers(2, 3, 4)
+
+# Identify the following items in that code:
+# function name                 = multiply_numbers
+# function arguments            = 2, 3, 4
+# function definition           = from def until result
+# function body                 = lines 2 and 3 
+# function parameters           = num1, num2, num3
+# function invocation           = multiply_numbers(2, 3, 4)
+# function return value         = 24
+# all identifiers               = (multiply_numbers, num1, num2, num3, result,  product)
