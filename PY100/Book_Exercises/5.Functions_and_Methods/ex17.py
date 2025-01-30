40cf46a8aeaf9b08e03be69618589b615cb02779
Iván E. Hernández T. <ivan@iamieht.com>
2025-01-30 15:55:32 +0100

    Functions and Methods: Ex17

---
+++ PY100/Book_Exercises/5.Functions_and_Methods/ex17.py
@@ -0,0 +1,19 @@
+# Which of the identifiers in the following program are function names? Which are method names? Which are built-in functions?
+
+def say(message):
+    print(f'==> {message}')
+
+string1 = input()
+string2 = input()
+
+say(max(string1.upper(), string2.lower()))
+
+# Function Names	
+# say	
+# Method Names	
+# upper	
+# lower	
+# Built-in Function Names
+# print
+# input
+# max
