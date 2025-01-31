9915d5856a1dd578435c8983f9a04049b0db1517
Iván E. Hernández T. <ivan@iamieht.com>
2025-01-31 14:17:22 +0100

    Create ex10.py
    Intro to Collections: Ex10

---
+++ PY100/Book_Exercises/7.Intro_2_Collections/ex10.py
@@ -0,0 +1,6 @@
+# Bob expects the following code to print the names in alphabetical order. Without running the code, can you say whether it will? Explain your answer.
+names = {'Chris', 'Clare', 'Karis', 'Karl',
+         'Max', 'Nick', 'Victor'}
+print(names)
+
+# This code probably won't print the names alphabetically. Sets, by definition, are unordered collections. Thus, the order in which members are shown when printing or iterating is arbitrary. Bob's code may print the names alphabetically on rare occasions, but it would do so only once every 5040 times.
