566188b3db9778e6bb1a34ad4090d9f8a2083dc7
Iván E. Hernández T. <ivan@iamieht.com>
2025-01-31 14:04:36 +0100

    Create ex3.py
    Intro to Collections: Ex3

---
+++ PY100/Book_Exercises/7.Intro_2_Collections/ex3.py
@@ -0,0 +1,8 @@
+# Identify at least 2 differences between lists and tuples. Identify at least 2 ways that lists and tuples are similar.
+
+# Differences:
+# Lists are mutable; tuples are immutable.
+# List literals use []; tuple literals use ().
+# Similarities:
+# Lists and tuples are both sequences. Sequences are ordered collections that can use numeric indexes to access the members.
+# Lists and tuples are heterogeneous; elements do not need to all be the same type.
