27520a9be21759c0be8e68e167f68ccacd959dce
Iván E. Hernández T. <ivan@iamieht.com>
2025-02-03 19:20:48 +0100

    Loops and Iterations: Ex2

---
+++ PY100/Book_Exercises/9.Loops_and_iterating/ex2.py
@@ -0,0 +1,8 @@
+# Modify the age.py program you wrote in Exercise 3 of the Input/Output chapter. The updated code should use a for loop to display the future ages.
+
+age = int(input('How old are you? '))
+print()
+print(f'You are {age} years old.')
+
+for num in range(10, 50, 10):
+    print(f'In {num} years, you will be {age + num} years old.')
