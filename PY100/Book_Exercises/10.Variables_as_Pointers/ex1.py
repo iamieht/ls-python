5e2abcc22d3474affb87dca939b9528c3ba5a559
Iván E. Hernández T. <ivan@iamieht.com>
2025-02-04 17:28:49 +0100

    Variables as Pointers: Ex1

---
+++ PY100/Book_Exercises/10.Variables_as_Pointers/ex1.py
@@ -0,0 +1,6 @@
+# In your own words, explain the difference between these two expressions.
+my_object1 == my_object2
+my_object1 is my_object2
+
+# Line 2 compares 2 objects for equality = referencing an object with the same values
+# Line 3 compares the identity of 2 objects. If both are referencing the same object in memory.
