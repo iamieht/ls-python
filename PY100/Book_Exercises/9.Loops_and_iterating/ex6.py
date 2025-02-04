bcceb114f32a2575bd9b503ce89192c2a6ae4b9d
Iván E. Hernández T. <ivan@iamieht.com>
2025-02-04 07:38:05 +0100

    Update ex6.py
    Ex6 - extended

--- PY100/Book_Exercises/9.Loops_and_iterating/ex6.py
+++ PY100/Book_Exercises/9.Loops_and_iterating/ex6.py
@@ -7,6 +7,8 @@ my_list = [
 ]
 
 new_list = []
+
+# Option 1
 for num in my_list:
     if num % 2 == 0:
         new_list.append('even')
@@ -14,3 +16,30 @@ for num in my_list:
         new_list.append('odd')
 
 print(new_list)
+
+# Option 2 (Ternary expression with comprehensions)
+my_list = [
+    1, 3, 6, 11,
+    4, 2, 4, 9,
+    17, 16, 0,
+]
+
+result = ['even' if number % 2 == 0 else 'odd'
+          for number in my_list]
+print(result)
+
+# Option 3 (helper function with comprehensions)
+my_list = [
+    1, 3, 6, 11,
+    4, 2, 4, 9,
+    17, 16, 0,
+]
+
+
+def odd_or_even(number):
+    return 'even' if number % 2 == 0 else 'odd'
+
+
+result = [odd_or_even(number)
+          for number in my_list]
+print(result)
