0f0d53cd7b89da2b074a6b63c48c3329d423430d
Iván E. Hernández T. <ivan@iamieht.com>
2025-02-05 13:48:48 +0100

    Create 7.find_substring.py
    Reading Documentation 2: find substring

---
+++ PY100/Python_Basics/2.Reading_Documentation2/7.find_substring.py
@@ -0,0 +1,4 @@
+# Using the official Python documentation, can you determine how to check whether a string contains a specific substring?
+# Membership test operations: https://docs.python.org/3/reference/expressions.html#in
+
+print('Hello' in 'Hello, World!')
