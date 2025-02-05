48c597072a9e9e73bf55e27ab3c59959dc1f1d36
Iván E. Hernández T. <ivan@iamieht.com>
2025-02-05 13:11:10 +0100

    Create 9.checking_data_types.py
    Checking Data Types

---
+++ PY100/Python_Basics/1.Reading_Documentation1/9.checking_data_types.py
@@ -0,0 +1,13 @@
+# Referring to the official Python documentation, how would you identify the data type of the following values?
+# 23.5
+# 'Call me Ishmael.'
+# False
+# 0
+# None
+# https://docs.python.org/3/library/functions.html#type
+
+print(type(23.5))
+print(type('Call me Ishmael.'))
+print(type(False))
+print(type(0))
+print(type(None))
