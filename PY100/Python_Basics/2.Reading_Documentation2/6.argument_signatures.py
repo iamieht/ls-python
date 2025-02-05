26e08a3ae9f58ed44d60f9993d367e6781429155
Iván E. Hernández T. <ivan@iamieht.com>
2025-02-05 13:45:43 +0100

    Create 6.argument_signatures.py
    Reading Documentation 2: Argument Signatures

---
+++ PY100/Python_Basics/2.Reading_Documentation2/6.argument_signatures.py
@@ -0,0 +1,4 @@
+# How many arguments does the str.join method expect? What happens if you call it with fewer or more arguments?
+# join expects a single iterable as an argument.
+print('-'.join())
+# If no argument is passed in, a TypeError exception is raised.
