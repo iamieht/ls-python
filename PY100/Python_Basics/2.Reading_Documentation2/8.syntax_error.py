7f54673c8d811c077f5c734fb052ee16efef2135
Iván E. Hernández T. <ivan@iamieht.com>
2025-02-05 13:51:20 +0100

    Create 8.syntax_error.py
    Reading Documentation 2: Syntax Error

---
+++ PY100/Python_Basics/2.Reading_Documentation2/8.syntax_error.py
@@ -0,0 +1,10 @@
+# What does a SyntaxError indicate? Try running the above code, then use the resulting error message to fix the error.
+speed_limit = 60
+current_speed = 80
+
+if current_speed > speed_limit:  # --> missing  colon
+    print('"People are so bad at driving cars that '
+          "computers don\'t have to be that good to be "
+          'much better." -- Marc Andreessen')
+
+# a Syntax error indicates that the Python interpreter cannot understand the code as there is an invalid construction.
