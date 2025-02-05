8934306ba25dd756624d6650877d5e1705a31ab2
Iván E. Hernández T. <ivan@iamieht.com>
2025-02-05 13:19:13 +0100

    Create 1.string_formatting.py
    Reading Documentation 2: String Formatting

---
+++ PY100/Python_Basics/2.Reading_Documentation2/1.string_formatting.py
@@ -0,0 +1,8 @@
+name = "Victor"
+profession = "programmer"
+
+# How can you print the string Hello, Victor. You are a programmer. using the str.format method? You should fill in the name and profession in a string literal that contains the rest of the text.
+print("Hello, {0}. You are a {1}.".format(name, profession))
+
+# How can you achieve the same result using an f-string?
+print(f'Hello, {name}. You are a {profession}.')
