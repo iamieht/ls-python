99fcbb850dad8c65e87bb02df7dd050252e898b1
Iván E. Hernández T. <ivan@iamieht.com>
2025-02-05 12:55:21 +0100

    Create 6.finding_index.py
    Finding Index

---
+++ PY100/Python_Basics/1.Reading_Documentation1/6.finding_index.py
@@ -0,0 +1,5 @@
+# How would you determine the index of the fruit "cherry" in this list?
+# https://docs.python.org/3/tutorial/datastructures.html
+
+fruits = ["apple", "banana", "cherry", "peach", "watermelon"]
+print(fruits.index('cherry'))
