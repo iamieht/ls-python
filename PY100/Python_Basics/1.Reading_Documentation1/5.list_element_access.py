3b9c73fc85aebac03bb3f9808dcc57ab18f84aaf
Iván E. Hernández T. <ivan@iamieht.com>
2025-02-05 12:52:17 +0100

    Create 5.list_element_access.py
    Python Basics: List Element Access

---
+++ PY100/Python_Basics/1.Reading_Documentation1/5.list_element_access.py
@@ -0,0 +1,9 @@
+# Locate the documentation for the list built-in object in Python Documentation.
+# https://docs.python.org/3/library/stdtypes.html#list
+# https://docs.python.org/3/tutorial/introduction.html#lists
+
+
+# How can we access the second element ('and') in the list ['fish', 'and', 'chips']?
+
+lst = ['fish', 'and', 'chips']
+print(lst[1])
