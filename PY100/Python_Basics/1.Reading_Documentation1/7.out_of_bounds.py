e1d08552dea62e5c78f97f144285cad3d82c7870
Iván E. Hernández T. <ivan@iamieht.com>
2025-02-05 12:59:35 +0100

    Create 7.out_of_bounds.py
    Out of Bounds

---
+++ PY100/Python_Basics/1.Reading_Documentation1/7.out_of_bounds.py
@@ -0,0 +1,6 @@
+# What happens if we take the list ['fish', 'and', 'chips'] and try to access the element at index position 10? First try to determine what will happen by consulting the documentation, then verify your understanding in the Python REPL.
+
+# The Python documentation, as of December 2023, doesn't mention what happens if the index position is out of bounds.
+
+lst = ['fish', 'and', 'chips']
+print(lst[10])
