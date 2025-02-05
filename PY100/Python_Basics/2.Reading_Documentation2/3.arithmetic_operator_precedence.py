5c53648275dfe747591964bc7da8c2a5bb215baf
Iván E. Hernández T. <ivan@iamieht.com>
2025-02-05 13:27:42 +0100

    Create 3.arithmetic_operator_precedence.py
    Reading Documentation 2: Arithmetic Operator Precedence

---
+++ PY100/Python_Basics/2.Reading_Documentation2/3.arithmetic_operator_precedence.py
@@ -0,0 +1,6 @@
+# Find the Python Documentation on operator precedence, and use it to determine the result of evaluating 4 * 5 + 3**2 / 10.
+# https://docs.python.org/3/reference/expressions.html#operator-summary
+
+print(4 * 5 + 3**2 / 10)
+print(20 + .9)
+print(20.9)
