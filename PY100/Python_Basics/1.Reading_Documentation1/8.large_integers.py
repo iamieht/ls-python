b9796b6df6083f6bee1d652c643a3bf9a70f5831
Iván E. Hernández T. <ivan@iamieht.com>
2025-02-05 13:07:03 +0100

    Create 8.large_integers.py
    Large Integers

---
+++ PY100/Python_Basics/1.Reading_Documentation1/8.large_integers.py
@@ -0,0 +1,8 @@
+# Using the Python documentation, determine how you can write large integers in a way that makes them easier to read.
+# https://docs.python.org/3/reference/lexical_analysis.html#integer-literals
+print(1_000_000_000)
+
+# Further exploration
+# Is it okay to write 1_987_654_321 as 19_87_65_4321? The Python interpreter understands both the same way, but the second one is harder to read and understand by a human.
+print(1_987_654_321)
+print(19_87_65_4321)
