88f879c93244b8a0d6e8cb6d2d6c7947afd2d4f0
Iván E. Hernández T. <ivan@iamieht.com>
2025-02-05 07:37:27 +0100

    Create ex2.py
    More Stuff: Ex2

---
+++ PY100/Book_Exercises/11.More_Stuff/ex2.py
@@ -0,0 +1,14 @@
+# Use the sqrt function from the math library to write some code that outputs the square root of 37. Try to write the code in three different ways.
+
+# Option 1
+import math
+
+# Option 2
+from math import sqrt
+
+# option 3 (import alias)
+import math as m
+
+print(math.sqrt(37))
+print(sqrt(37))
+print(m.sqrt(37))
