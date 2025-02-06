314d3dbb8e78d007c85c4f972db14da9cc2dda5f
Iván E. Hernández T. <ivan@iamieht.com>
2025-02-06 08:27:50 +0100

    Create 1.truthy_vs_falsy.py
    Conditionals: falsy vs truthy

---
+++ PY100/Python_Basics/4.Conditionals/1.truthy_vs_falsy.py
@@ -0,0 +1,7 @@
+# Without looking at your notes or the official documentation, try to recall all values that count as falsy in Python.
+# - empty collections: [], {}, (), set()
+# - any numeric zero-values
+# - None
+# - ''
+# - False
+# range(0)
