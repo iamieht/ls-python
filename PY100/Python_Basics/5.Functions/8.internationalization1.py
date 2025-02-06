438493d6d493d1fbe2390fd0b4452ccec3f9c1a8
Iván E. Hernández T. <ivan@iamieht.com>
2025-02-06 15:03:22 +0100

    Create 8.internationalization1.py
    Functions: Internationalization 1

---
+++ PY100/Python_Basics/5.Functions/8.internationalization1.py
@@ -0,0 +1,25 @@
+# Use Python's control structures to create a function that takes an ISO 639-1 language code and returns a greeting in that language.
+def greet(isolang):
+    match isolang:
+        case 'en':
+            return 'Hi!'
+        case 'fr':
+            return 'Salut!'
+        case 'pt':
+            return 'Olá!'
+        case 'de':
+            return 'Hallo!'
+        case 'sv':
+            return 'Hej!'
+        case 'af':
+            return 'Haai!'
+        case _:
+            return 'Invalid ISO Language Code'
+
+
+print(greet('en'))         # Hi!
+print(greet('fr'))         # Salut!
+print(greet('pt'))         # Olá!
+print(greet('de'))         # Hallo!
+print(greet('sv'))         # Hej!
+print(greet('af'))         # Haai!
