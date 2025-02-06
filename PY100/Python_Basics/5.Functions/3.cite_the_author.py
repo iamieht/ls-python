3b99988888d9d926c08f7a6c89db571607d26021
Iván E. Hernández T. <ivan@iamieht.com>
2025-02-06 14:45:14 +0100

    Create 3.cite_the_author.py
    Functions: Cite the Author

---
+++ PY100/Python_Basics/5.Functions/3.cite_the_author.py
@@ -0,0 +1,7 @@
+# Implement a function named cite that takes two string arguments: the author of the quote and what they said. It then prints the quote, as shown below.
+def cite(author, message):
+    print(f'{author} said: "{message}"')
+
+
+cite('Bruce Eckel', 'Python is executable pseudocode.')
+# Bruce Eckel said: "Python is executable pseudocode."
