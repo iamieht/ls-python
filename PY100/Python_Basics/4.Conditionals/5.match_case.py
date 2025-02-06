fd983a58ec87f8a430e6bfe975c0c9e405b24296
Iván E. Hernández T. <ivan@iamieht.com>
2025-02-06 08:40:51 +0100

    Create 5.match_case.py
    Conditionals: match-case

---
+++ PY100/Python_Basics/4.Conditionals/5.match_case.py
@@ -0,0 +1,16 @@
+# Without running it, determine what it will print.
+animal = 'horse'
+
+match animal:
+    case 'duck':
+        print('quack')
+    case 'squirrel':
+        print('nook nook')
+    case 'horse':
+        print('neigh')
+    case 'bird':
+        print('tweet tweet')
+    case _:
+        print('*cricket*')
+
+# neigh
