7eb13cdeb5dde07cf0f595a6e52a15e6c86b5655
Iván E. Hernández T. <ivan@iamieht.com>
2025-02-07 10:06:27 +0100

    Create 8.what_is_my_value_part8.py
    What's my value? (Part 8)

---
+++ PY100/Python_Basics/6.Variable_Scope/8.what_is_my_value_part8.py
@@ -0,0 +1,7 @@
+# What will the following code do and why? Don't run it until you have tried to answer.
+# print(greeting)
+
+# greeting = 'Hello world!'
+
+# a NameError is raised as the global variable greeting is being passed as an argument into the print function but it is not yet initialized.
+# In Python, you must initialize a variable before you use it; otherwise, it raises a NameError indicating the name hasn't been defined.
