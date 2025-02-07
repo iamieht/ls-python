267d7de68b8e7326657c39d99b10eae2b85b98dd
Iván E. Hernández T. <ivan@iamieht.com>
2025-02-07 16:29:58 +0100

    Lists: Travel

---
+++ PY100/Python_Basics/8.Lists/9.travel.py
@@ -0,0 +1,15 @@
+# Write a function that, without using the built-in in operator, checks whether a specific destination is included within destinations. For example: When checking whether 'Barcelona' is contained in destinations, the expected output is True, whereas the expected output for 'Nashville' is False.
+def contains(destination, destinations):
+    for city in destinations:
+        if city == destination:
+            return True
+        
+    return False
+
+destinations = ['Prague', 'London', 'Sydney', 'Belfast',
+                'Rome', 'Aruba', 'Paris', 'Bora Bora',
+                'Barcelona', 'Rio de Janeiro', 'Marrakesh',
+                'New York City']
+
+print(contains('Barcelona', destinations))  # True
+print(contains('Nashville', destinations))  # False
