760cec34c54b0da7e541bfd71836693054b5ae04
Iván E. Hernández T. <ivan@iamieht.com>
2025-02-10 08:39:55 +0100

    Create 5.whats_my_length.py
    What's My Length?

---
+++ PY100/Python_Basics/9.Dictionaries/5.whats_my_length.py
@@ -0,0 +1,8 @@
+# Calculate and print the number of key/value pairs in the following dictionary:
+car = {
+    'type':  'sedan',
+    'color': 'blue',
+    'year':  2003,
+}
+
+print(f'The number of key value/pairs in {car} is {len(car)}')
