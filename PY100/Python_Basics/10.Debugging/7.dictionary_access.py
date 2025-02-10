e3dc6b4acc60bde573a05a6691bbcf69361a696f
Iván E. Hernández T. <ivan@iamieht.com>
2025-02-10 09:36:46 +0100

    Create 7.dictionary_access.py
    Dictionary Access

---
+++ PY100/Python_Basics/10.Debugging/7.dictionary_access.py
@@ -0,0 +1,5 @@
+# You are trying to access a value in a dictionary, but the code is giving you an error. Can you change line 4 so that it prints "Unknown" instead of raising an error?
+info = {'name': 'Srdjan', 'age': 38}
+
+# print(info['city'])
+print(info.get('city', 'Unknown'))
