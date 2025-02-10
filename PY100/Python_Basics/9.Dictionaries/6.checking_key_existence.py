6fe286fd18b9f997c5f511684d82874c880069c5
Iván E. Hernández T. <ivan@iamieht.com>
2025-02-10 08:43:25 +0100

    Create 6.checking_key_existence.py
    Checking Key Existence

---
+++ PY100/Python_Basics/9.Dictionaries/6.checking_key_existence.py
@@ -0,0 +1,8 @@
+# Check whether the keys 'name' and 'grade' exist in the following dictionary:
+student = {
+    'id': 123,
+    'grade': 'B',
+}
+
+print(f'Key "name" exist in {student}? {"name" in student}')
+print(f'Key "grade" exist in {student}? {"grade" in student}')
