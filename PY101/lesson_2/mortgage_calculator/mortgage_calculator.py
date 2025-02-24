9baf7dbcbf13309b33cf5a591f18aa2aa7fe3f75
Iván E. Hernández T. <ivan@iamieht.com>
2025-02-24 08:09:24 +0100

    Create mortgage_calculator.py
    Mortgage Calculator - PEDAC

---
+++ PY101/lesson_2/mortgage_calculator/mortgage_calculator.py
@@ -0,0 +1,45 @@
+# Problem
+# build a mortgage calculator (or car payment calculator -- it's the same thing) that determines the monthly payment assuming that interest is compounded monthly.
+
+# input
+#   - loan amount
+#   - APR = Annual Percentage Rate
+#   - loan duration
+# output
+#   - Monthly payment = loan_amount * (monthly_interest_rate / (1 - (1 + monthly_interest_rate) ** (-loan_duration_months)))
+# calculate
+#   - monthly_interest_rate (APR / 12)
+#   - loan duration in months
+
+# Examples / Test Cases
+
+# Data Structures
+# - float
+# - strings
+
+# Algorithm
+# Create a .json file for the messages with internationalization support (en / es)
+# Create a function prompt to define a user prompt
+#   - use a message key to retrieve msg from .json file
+# Create a function to capture user input
+#   - get_input()
+# Create a function to check user input (amounts)
+#   - is_valid_amount()
+# Create a function to check user input (APR)
+#   - Ask for a %
+#   - is_valid_apr()
+# Create a function to check user input (loan duration)
+#   - is_valid_loan()
+# Create a function to calculate interest rate (monthly)
+#   - calculate_interest_rate()
+# Crete a function to calculate loan duration in months
+#   - calculate_loan_duration()
+# Create a function to calculate the mortgage (monthly payment)
+#   - calculate_mortgage()
+# Create a main function
+#   - print Welcome Screen
+#   - Ask for loan amount ($)
+#   - Ask for loan term (Years / Months)
+#   - Ask for Interest Rate (%)
+
+# Code
