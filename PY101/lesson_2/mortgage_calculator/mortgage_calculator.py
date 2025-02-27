19f2a88208ff453b3c911393c6d0040ff92a438b
Iván E. Hernández T. <ivan@iamieht.com>
2025-02-27 08:49:59 +0100

    Update mortgage_calculator.py
    Fixing lines too long

--- PY101/lesson_2/mortgage_calculator/mortgage_calculator.py
+++ PY101/lesson_2/mortgage_calculator/mortgage_calculator.py
@@ -136,20 +136,33 @@ def mortgage_calculator(interest_rate, loan_amount, loan_term):
     total_payments = monthly_payment * loan_term
     total_interest = total_payments - loan_amount
 
-    display_results(loan_amount, monthly_payment, loan_term,
-                    interest_rate, monthly_interest_rate, total_payments, total_interest)
+    display_results(loan_amount, monthly_payment,
+                    loan_term, interest_rate,
+                    monthly_interest_rate, total_payments,
+                    total_interest)
 
 
-def display_results(loan_amount, monthly_payment, loan_term, interest_rate, monthly_interest_rate, total_payments, total_interest):
+def display_results(loan_amount, monthly_payment,
+                    loan_term, interest_rate,
+                    monthly_interest_rate, total_payments,
+                    total_interest):
+
     print()
     print(messages('results', LANG))
-    print(f'{messages("label_loan_amount", LANG).ljust(30)} = ${loan_amount:,.2f}')
-    print(f'{messages("label_loan_term", LANG).ljust(30)} = {loan_term}')
-    print(f'{messages("label_apr", LANG).ljust(30)} = % {interest_rate}')
-    print(f'{messages("label_mpr", LANG).ljust(30)} = % {monthly_interest_rate:.4f}')
-    print(f'{messages("label_m_payment", LANG).ljust(30)} = ${monthly_payment:,.2f}')
-    print(f'{messages("label_t_payments", LANG).ljust(30)} = ${total_payments:,.2f}')
-    print(f'{messages("label_interests", LANG).ljust(30)} = ${total_interest:,.2f}')
+    print(f'''{messages("label_loan_amount", LANG).ljust(30)}'
+          '= ${loan_amount: , .2f}''')
+    print(f'''{messages("label_loan_term", LANG).ljust(30)}'
+          '={loan_term}''')
+    print(f'''{messages("label_apr", LANG).ljust(30)}'
+          '= % {interest_rate}''')
+    print(f'''{messages("label_mpr", LANG).ljust(30)}'
+          '=% {monthly_interest_rate: .4f}''')
+    print(f'''{messages("label_m_payment", LANG).ljust(30)}'
+          '= ${monthly_payment: , .2f}''')
+    print(f'''{messages("label_t_payments", LANG).ljust(30)}'
+          '= ${total_payments: , .2f}''')
+    print(f'''{messages("label_interests", LANG).ljust(30)}'
+          '= ${total_interest: , .2f}''')
     print(messages('hr'))
 
 
