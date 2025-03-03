5a0888557a54865ef75b98c8a32505d4c42e2db9
Iván E. Hernández T. <ivan@iamieht.com>
2025-03-03 17:54:21 +0100

    RPS - Part 2

--- PY101/lesson_2/rps/rps.py
+++ PY101/lesson_2/rps/rps.py
@@ -5,14 +5,40 @@ VALID_CHOICES = ['rock', 'paper', 'scissors']
 def prompt(message):
     print(f'==> {message}')
 
-prompt(f'Choose one: {", ".join(VALID_CHOICES)}')
-choice = input()
+def display_winner(player, computer):
+    if ((player == 'rock' and computer == 'scissors') or
+        (player == 'paper' and computer == 'rock') or
+        (player == 'scissors' and computer == 'paper')):
+        prompt('You win!')
+    elif ((player == 'rock' and computer == 'paper') or
+        (player == 'paper' and computer == 'scissors') or
+        (player == 'scissors' and computer == 'rock')):
+        prompt('Computer wins!')
+    else:
+        prompt("It's a tie!")
 
-while choice not in VALID_CHOICES:
-    prompt("That's not a valid choice")
+while True:
+    prompt(f'Choose one: {", ".join(VALID_CHOICES)}')
     choice = input()
 
-computer_choice = random.choice(VALID_CHOICES)
+    while choice not in VALID_CHOICES:
+        prompt("That's not a valid choice")
+        choice = input()
 
-prompt(f'You chose {choice}, computer chose {computer_choice}')
+    computer_choice = random.choice(VALID_CHOICES)
 
+    prompt(f'You chose {choice}, computer chose {computer_choice}')
+
+    display_winner(choice, computer_choice)
+
+    prompt('Do you want to play again? (y/n)?')
+    answer = input().lower()
+    while True:
+        if answer.startswith('y') or answer.startswith('n'):
+            break
+
+        prompt('Please enter "y" or "n".')
+        answer = input().lower()
+
+    if answer[0] == 'n':
+        break
