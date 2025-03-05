9b8e0d843aa2be90867750ec3f06729796bacf0c
Iván E. Hernández T. <ivan@iamieht.com>
2025-03-05 18:35:53 +0100

    Problems Easy 1: question 8

---
+++ PY101/lesson_3/Easy1/question8.py
@@ -0,0 +1,5 @@
+# How can we add multiple items to our list (e.g., 'Dino' and 'Hoppy')? 
+
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "Bambam", "Pebbles"]
+flintstones.extend(["Dino", "Hoppy"])
+print(flintstones)
