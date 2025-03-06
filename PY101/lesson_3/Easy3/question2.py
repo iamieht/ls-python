a963bd685086ba5eb1c2d2b1148b04190db965af
Iván E. Hernández T. <ivan@iamieht.com>
2025-03-06 13:37:09 +0100

    Create question2.py
    Easy3: question 2

---
+++ PY101/lesson_3/Easy3/question2.py
@@ -0,0 +1,4 @@
+# What will the following code output?
+print([1, 2, 3] + [4, 5])
+
+# [1, 2, 3, 4, 5]. In Python, you can use the + operator to concatenate two lists. This operation merges the second list into the first one, producing a new combined list.
