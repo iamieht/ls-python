774cba2ca0a94d63ff5abba24c90713f9a0fe893
Iván E. Hernández T. <ivan@iamieht.com>
2025-03-06 13:44:26 +0100

    Create question5.py
    Easy3: question 5

---
+++ PY101/lesson_3/Easy3/question5.py
@@ -0,0 +1,13 @@
+# The following function unnecessarily uses two return statements to return boolean values. Can you rewrite this function so it only has one return statement and does not explicitly use either True or False?
+# def is_color_valid(color):
+#     if color == "blue" or color == "green":
+#         return True
+#     else:
+#         return False
+
+def is_color_valid(color):
+    return color == "blue" or color == "green"
+
+print(is_color_valid("blue"))
+print(is_color_valid("green"))
+print(is_color_valid("black"))
