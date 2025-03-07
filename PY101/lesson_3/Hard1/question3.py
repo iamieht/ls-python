56c82b4d62bc79661678f8b19dc782fcfe1e3595
Iván E. Hernández T. <ivan@iamieht.com>
2025-03-07 10:01:33 +0100

    Create question3.py
    Hard1: question3

---
+++ PY101/lesson_3/Hard1/question3.py
@@ -0,0 +1,48 @@
+# Given the following similar sets of code, what will each code snippet print?
+# A)
+# def mess_with_vars(one, two, three):
+#     one = two
+#     two = three
+#     three = one
+
+# one = ["one"]
+# two = ["two"]
+# three = ["three"]
+
+# mess_with_vars(one, two, three)
+
+# print(f"one is: {one}")         # ["one"]
+# print(f"two is: {two}")         # ["two"]
+# print(f"three is: {three}")     # ["three"]
+
+#B)
+# def mess_with_vars(one, two, three):
+#     one = ["two"]
+#     two = ["three"]
+#     three = ["one"]
+
+# one = ["one"]
+# two = ["two"]
+# three = ["three"]
+
+# mess_with_vars(one, two, three)
+
+# print(f"one is: {one}")     # ["one"]
+# print(f"two is: {two}")     # ["two"]
+# print(f"three is: {three}") # ["three"]
+
+#C)
+def mess_with_vars(one, two, three):
+    one[0] = "two"
+    two[0] = "three"
+    three[0] = "one"
+
+one = ["one"]
+two = ["two"]
+three = ["three"]
+
+mess_with_vars(one, two, three)
+
+print(f"one is: {one}")     # ['two']
+print(f"two is: {two}")     # ['three']
+print(f"three is: {three}") # ['one']
